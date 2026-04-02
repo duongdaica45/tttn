7c158f752302974238b95abdccb00ff4bf16eaee
duongdaica45 <ntduong.ti@gmail.com>
2026-04-03 00:34:26 +0700

    add dockerfile

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\ForceHttps;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -11,14 +11,20 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
+        // 1. Luôn để Cors lên đầu (Đã đúng)
         $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
 
+        // 2. QUAN TRỌNG NHẤT: Tin tưởng Proxy của Render
+        // Dòng này sẽ dập tắt lỗi "Redirected too many times"
+        $middleware->trustProxies(at: '*');
+
+        // 3. XÓA BỎ ForceHttps::class (Vì nó gây vòng lặp trên Render)
         $middleware->api(append: [
-            ForceHttps::class, 
+            // Đừng cho ForceHttps vào đây nữa
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })
     ->create();
