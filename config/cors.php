37f2b1378788aabf9d62c64bf2893451bff7ef85
duongdaica45 <ntduong.ti@gmail.com>
2026-04-03 12:33:47 +0700

    add file

--- config/cors.php
+++ config/cors.php
@@ -5,9 +5,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*' ],
+    // Thay vì để dấu sao, hãy điền chính xác domain GitHub của bạn
+    'allowed_origins' => ['https://duongdaica45.github.io'], 
 
-    'allowed_origins_patterns' => ['*'],
+    // Quan trọng: Để mảng rỗng nếu không dùng pattern
+    'allowed_origins_patterns' => [], 
 
     'allowed_headers' => ['*'],
 
