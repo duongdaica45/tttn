d7d402ecb4cf8f12dd7c1057b6345886289b8ea3
duongdaica45 <ntduong.ti@gmail.com>
2026-04-03 13:06:49 +0700

    add file

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,13 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\NhanVienController;
 
 
+Route::options('{any}', function() {
+    return response()->json([], 200)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With');
+})->where('any', '.*');
+
 //Đăng nhập
 Route::post('/login', [AuthController::class, 'login']);
 //Admin
