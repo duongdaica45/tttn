e9d6e4678ca2c145bbe4fb0017e453c401e20244
duongdaica45 <ntduong.ti@gmail.com>
2026-04-14 20:59:57 +0700

    add last

--- app/Http/Middleware/HandleCorsHeaders.php
+++ app/Http/Middleware/HandleCorsHeaders.php
@@ -12,8 +12,8 @@ class HandleCorsHeaders
     {
         if ($request->isMethod('OPTIONS')) {
             return response('', 204)
-                //                ->header('Access-Control-Allow-Origin', 'https://duongdaica45.github.io')
-                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Origin', 'https://duongdaica45.github.io')
+                //->header('Access-Control-Allow-Origin', '*')
                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
                 ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin')
                 ->header('Access-Control-Allow-Credentials', 'true');
@@ -22,8 +22,8 @@ class HandleCorsHeaders
         $response = $next($request);
 
         // Ép Header vào phản hồi thực tế
-        //        $response->headers->set('Access-Control-Allow-Origin', 'https://duongdaica45.github.io');
-        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Origin', 'https://duongdaica45.github.io');
+       // $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
