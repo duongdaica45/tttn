b04c600290223dc3694e64eb0d6a33d4c7c5870f
duongdaica45 <ntduong.ti@gmail.com>
2026-04-17 08:28:31 +0700

    add last

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\App;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,7 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
-
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        Carbon::setLocale('vi');
         // Chuyển đoạn code này xuống hàm boot
         if (App::environment(['production', 'staging'])) {
             URL::forceScheme('https');
